0852bb93c827fa5eed5fd10c1aeeed7921bfe8af
Nipaaaa1 <Frostone224@gmail.com>
2024-02-28 15:59:38 +0700

    refactor: add Header component

---
+++ components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <header class="relative w-full p-8 bg-primary-intro-dark-blue text-center text-white flex flex-col gap-8 items-center">
+        <NuxtImg src="/illustration-intro.png" alt="People carrying document into file illustration." />
+        <h1 class="font-raleway text-white font-bold text-[1.7rem] tracking-wide text-center leading-relaxed">All your files in one secure location, accessible anywhere.</h1>
+        <p class="font-open-sans">Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers.</p>
+        <button class="px-20 w-max rounded-full py-4 bg-gradient-to-br from-accent-cyan to-accent-blue font-open-sans font-bold" type="button">Get Started</button>
+    </header>    
+</template>
