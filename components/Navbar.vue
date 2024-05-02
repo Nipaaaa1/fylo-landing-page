a03c1b9bc8060c028885ff8910507d18567cf2f0
Nipaaaa1 <Frostone224@gmail.com>
2024-05-02 13:09:09 +0700

    feat: change links color when hover

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,11 +5,11 @@
     >
       <NuxtImg class="h-8 w-max md:h-12" src="/images/logo.svg" alt="Logo." />
       <ul
-        class="flex gap-6 font-raleway text-sm tracking-wide text-white/80 *:cursor-pointer md:gap-14"
+        class="flex gap-6 font-raleway text-sm tracking-wide text-white/80 *:cursor-pointer"
       >
-        <li>Features</li>
-        <li>Team</li>
-        <li>Sign In</li>
+        <li class="transition ease-in-out hover:text-white">Features</li>
+        <li class="transition ease-in-out hover:text-white">Team</li>
+        <li class="transition ease-in-out hover:text-white">Sign In</li>
       </ul>
     </div>
   </nav>
